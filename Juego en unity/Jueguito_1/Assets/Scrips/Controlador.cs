a639671d42c2e0cef89b31f3f7455a6a75d538cc
MauroAguirre <42445651+MauroAguirre@users.noreply.github.com>
2018-12-29 13:24:19 -0300

    Initial commit

---
+++ Juego en unity/Jueguito_1/Assets/Scrips/Controlador.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controlador : MonoBehaviour
+{
+    public void CambiarEscena(string escena)
+    {
+        print("Cambiando a escena: "+ escena);
+        SceneManager.LoadScene(escena);
+    }
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}
