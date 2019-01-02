0d83e4c49b3a97b8185b111e43f0c343ea2a1c21
MauroAguirre <42445651+MauroAguirre@users.noreply.github.com>
2019-01-02 14:20:29 -0300

    cambios y prototipo 2
    Agrege un personaje que encontre en internet, podemos usarlo como referencia

--- Juego en unity/Jueguito_1/Assets/Personajes/Jugador/Jugador_controles.cs
+++ Juego en unity/Jueguito_1/Assets/Personajes/Jugador/Jugador_controles.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Jugador_controles : MonoBehaviour
 {
+    public bool direccion = true;
     public float velocidad_movimiento = 5f;
-    public float velocidad_salto = 5f;
+    public float potencia_salto = 60f;
     private Animator animator;
+    private SpriteRenderer sprite;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -20,29 +23,46 @@ public class Jugador_controles : MonoBehaviour
         {
             case 0:
             case 1:
-                if (Input.GetKey(KeyCode.A))
+                animator.SetInteger("estado", 0);
+                //ataque
+                if (Input.GetKey(KeyCode.G))
                 {
-                    this.transform.position += new Vector3(-velocidad_movimiento * Time.deltaTime, 0, 0);
-                    animator.SetInteger("estado", 1);
+                    animator.SetInteger("estado", 2);
+                    Invoke("terminaAtaque", 1);
                 }
                 else
                 {
-                    if (Input.GetKey(KeyCode.D))
+                    //movimiento
+                    if (Input.GetKey(KeyCode.A))
                     {
-                        this.transform.position += new Vector3(+velocidad_movimiento * Time.deltaTime, 0, 0);
+                        this.transform.position += new Vector3(-velocidad_movimiento * Time.deltaTime, 0, 0);
                         animator.SetInteger("estado", 1);
+                        if (!direccion)
+                        {
+                            direccion = true;
+                            sprite.flipX = true;
+                        }
                     }
                     else
                     {
-                        if (Input.GetKey(KeyCode.G)||(Input.GetKey(KeyCode.G)&& Input.GetKey(KeyCode.D)|| Input.GetKey(KeyCode.A)))
+                        if (Input.GetKey(KeyCode.D))
                         {
-                            animator.SetInteger("estado", 2);
-                        }
-                        else
-                        {
-                            animator.SetInteger("estado", 0);
+                            this.transform.position += new Vector3(+velocidad_movimiento * Time.deltaTime, 0, 0);
+                            animator.SetInteger("estado", 1);
+                            if (direccion)
+                            {
+                                direccion = false;
+                                sprite.flipX = false;
+                            }
                         }
                     }
+                }
+                //saltar
+                if (Input.GetKey(KeyCode.W))
+                {
+                    this.transform.position += new Vector3(0, +potencia_salto * Time.deltaTime, 0);
+                    animator.SetInteger("estado", 2);
+                    Invoke("terminaAtaque", 1); ;
                 }
                 break;
             case 2:
@@ -53,6 +73,5 @@ public class Jugador_controles : MonoBehaviour
     public void terminaAtaque()
     {
         animator.SetInteger("estado",0);
-        //animator.state
     }
 }
